2030491fe22ddca9a03daa2476a69810c262465f
Alexandre <a.noel054@gmail.com>
2026-06-09 16:46:22 +0200

    intialisation du back et dépendances springboot

---
+++ back/masterclass-back/src/main/java/fr/fil/masterclass_back/MasterclassBackApplication.java
@@ -0,0 +1,13 @@
+package fr.fil.masterclass_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterclassBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterclassBackApplication.class, args);
+	}
+
+}
---
+++ back/masterclass-back/src/test/java/fr/fil/masterclass_back/MasterclassBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fil.masterclass_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterclassBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
