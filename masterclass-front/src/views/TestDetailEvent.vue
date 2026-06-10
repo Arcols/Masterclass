9541f627bef78ea1876143cf1b026c91a5ceefd6
Damien Kowalski <damien.kowalski@gmail.com>
2026-06-10 17:52:38 +0200

    feat: Add Detail Event + git ignore

---
+++ masterclass-front/src/views/TestDetailEvent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import DetailEvent from "@/components/DetailEvent.vue";
+
+const event = ref({
+  id: 1,
+  title: "Test Vue",
+  type: "homework",
+  start: new Date(),
+  end: new Date(),
+  group: "B3",
+  location: "Salle 101",
+  description: "Ceci est un test",
+  isDone: false,
+});
+</script>
+
+<template>
+  <DetailEvent :event="event" />
+</template>
+
+<style scoped></style>
