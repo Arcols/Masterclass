a263c8be43789212d9c3c264be2ec7bd3300d98e
Arthur Colson <120205594+Arcols@users.noreply.github.com>
2026-06-10 14:49:08 +0200

    feat: creating login page and deleting unusable pages

--- masterclass-front/src/App.vue
+++ masterclass-front/src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Calendar from './components/calendar/Calendar.vue'
 </script>
 
 <template>
-  <Calendar />
+  <RouterView />
 </template>
+
+<style scoped></style>
---
+++ masterclass-front/src/views/LoginView.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<body>
+  <h1>Connexion</h1>
+  <div class="login-form">
+    <form>
+      <div class="email-form">
+        <label for="email">Email</label>
+        <input type="text" placeholder="Ex:name.surname@imt-atlantique.net">
+      </div>
+      <div class="password-form">
+        <label for="password">Password</label>
+        <input type="password" placeholder="••••••••••••••••••••"></input>
+      </div>
+      <button class="login-button" type="submit">Login</button>
+    </form>
+  </div>
+</body>
+</template>
+
+<style scoped>
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+form {
+  width:300px;
+}
+
+.email-form, .password-form {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  text-align: left;
+}
+.login-button {
+  padding: 10px 20px;
+  background-color: #00786F;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 100%;
+}
+
+input{
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+input::placeholder {
+  color: gray;
+}
+
+</style>
