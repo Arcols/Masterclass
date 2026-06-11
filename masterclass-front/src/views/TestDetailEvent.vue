b9c33db19353f8597332a037d9a79c92dca832f2
Damien Kowalski <damien.kowalski@gmail.com>
2026-06-11 13:25:39 +0200

    feat: ajout de heroicons + vuetify + amélioration DetailEventModal

--- masterclass-front/src/views/TestDetailEvent.vue
+++ masterclass-front/src/views/TestDetailEvent.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import DetailEvent from "@/components/DetailEvent.vue";
+import DetailEventModal from "@/components/DetailEventModal.vue";
 
 const event = ref({
   id: 1,
-  title: "Test Vue",
+  title: "Showcase Yanis",
   type: "homework",
-  start: new Date(),
-  end: new Date(),
-  group: "B3",
-  location: "Salle 101",
-  description: "Ceci est un test",
+  start: new Date("2026-06-11T14:00:00"),
+  end: new Date("2026-06-11T16:30:00"),
+  group: "FIL A1",
+  location: "MDE",
+  description: "Préparez vous pour l’évent de l’année organisé par votre rappeur préféré (d’après une étude réalisée sur une personne). Ne ratez pas le concert privé, happening garantis.",
   isDone: false,
 });
+
+const isOpen = ref(true);
 </script>
 
 <template>
-  <DetailEvent :event="event" />
+  <DetailEventModal v-if="isOpen" :event="event" @close="isOpen = false" />
 </template>
-
-<style scoped></style>
