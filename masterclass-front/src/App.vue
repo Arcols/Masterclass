d3c08582d9ad03f2f5eb09dba426e86cd959028f
BOURREAUQuentin <bourreauquentin37800@gmail.com>
2026-06-11 11:04:26 +0200

    feat(ui): Création d'une carte pour un évènement

--- masterclass-front/src/App.vue
+++ masterclass-front/src/App.vue
@@ -1,6 +1,19 @@
+<style scoped></style>
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import Header from '@/components/Header.vue'
+import { ref } from 'vue'
+import EventCard from '@/components/EventCard.vue'
+import jsonMock from '@/mocks/events.json'
+
+// On rend le mock réactif pour pouvoir tester les clics sur la checkbox
+const events = ref(jsonMock)
+
+// Met à jour l'état de l'événement lors du clic
+const updateStatus = (id: string, newValue: boolean) => {
+  const targetEvent = events.value.find((e) => e.id === id)
+  if (targetEvent) {
+    targetEvent.isCompleted = newValue
+  }
+}
 </script>
 
 <template>
