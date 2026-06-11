060d0e0bdc121dde4fb2534ada8ec000a3b64b46
BOURREAUQuentin <bourreauquentin37800@gmail.com>
2026-06-11 14:10:00 +0200

    fix(ui): Correctifs Rebasage Damien

--- masterclass-front/src/router/index.ts
+++ masterclass-front/src/router/index.ts
@@ -1,9 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
-<<<<<<< HEAD
-import DetailEvent from '../components/DetailEvent.vue'
-=======
->>>>>>> 628e67b80458adafeb3dc08071bcc0ede9da9e05
 import TestDetailEvent from '@/views/TestDetailEvent.vue'
 
 const router = createRouter({
