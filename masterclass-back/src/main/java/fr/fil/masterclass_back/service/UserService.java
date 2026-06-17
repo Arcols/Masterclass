153711b15d407ab292bc6a33453b9e747933649d
Damien Kowalski <damien.kowalski@gmail.com>
2026-06-17 11:16:22 +0200

    feat: Modification des informations d'un User

--- masterclass-back/src/main/java/fr/fil/masterclass_back/service/UserService.java
+++ masterclass-back/src/main/java/fr/fil/masterclass_back/service/UserService.java
@@ -1,5 +1,7 @@
 package fr.fil.masterclass_back.service;
 
+import fr.fil.masterclass_back.model.Group;
+import fr.fil.masterclass_back.repository.GroupRepository;
 import fr.fil.masterclass_back.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import fr.fil.masterclass_back.model.User;
@@ -10,12 +12,42 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, GroupRepository groupRepository) {
         this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
     }
 
-    public Optional<User> getUserById(String id) {
+    public Optional<User> GetUserById(String id)
+    {
         return userRepository.findById(id);
+    }
+
+    public Optional<User> UpdateUserById(String id, User userDetails) {
+        return userRepository.findById(id).map(existingUser -> {
+            if (userDetails.getUseFirstname() != null) {
+                existingUser.setUseFirstname(userDetails.getUseFirstname());
+            }
+            if (userDetails.getUseLastname() != null) {
+                existingUser.setUseLastname(userDetails.getUseLastname());
+            }
+            if (userDetails.getUseMail() != null) {
+                existingUser.setUseMail(userDetails.getUseMail());
+            }
+            if (userDetails.getUseDescription() != null) {
+                existingUser.setUseDescription(userDetails.getUseDescription());
+            }
+            if (userDetails.getGroups() != null) {
+                List<String> groupIds = userDetails.getGroups().stream()
+                        .map(Group::getGroId)
+                        .collect(Collectors.toList());
+
+                List<Group> realGroups = groupRepository.findAllById(groupIds);
+                existingUser.setGroups(realGroups);
+            }
+
+            return userRepository.save(existingUser);
+        });
     }
 }
