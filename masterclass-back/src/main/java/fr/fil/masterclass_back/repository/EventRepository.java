0ec531800b1ada8d7290ed9a9516c818fd7c8897
Arthur Colson <120205594+Arcols@users.noreply.github.com>
2026-06-18 14:30:55 +0200

    :sparkles: feat(event): ajout de l'endpoint todolist + modif import et schéma
    # Conflicts:
    #	masterclass-back/src/main/resources/import.sql
    #	masterclass-back/src/main/resources/schema.sql

--- masterclass-back/src/main/java/fr/fil/masterclass_back/repository/EventRepository.java
+++ masterclass-back/src/main/java/fr/fil/masterclass_back/repository/EventRepository.java
@@ -2,6 +2,15 @@ package fr.fil.masterclass_back.repository;
 
 import fr.fil.masterclass_back.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface EventRepository extends JpaRepository<Event, String> {
+    @Query("SELECT e FROM Event e WHERE e.eveType IN :types AND e.eveDate > :date ORDER BY e.eveDate ASC")
+    List<Event> findFutureByTypes(@Param("types") List<String> types, @Param("date") LocalDate date);
 }
